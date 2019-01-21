574554d8bddc32bd8878a6a0dd6f01e888251a59
jcastro61 <jcastro61@hotmail.com>
2019-01-21 13:40:44 -0500

    reformatted

--- utils/database_utilities.py
+++ utils/database_utilities.py
@@ -1,7 +1,8 @@
 def create_database(database_path: str):
-    #todo add code to create sqlite db
+    # todo add code to create sqlite db
     pass
 
+
 def save_words_to_database(word_list: list):
-    #todo save words to database
+    # todo save words to database
     pass
--- utils/url_utilities.py
+++ utils/url_utilities.py
@@ -1,11 +1,13 @@
 def load_urls_from_file(file_path: str):
-    #todo: add code to read text file
+    # todo: add code to read text file
     pass
+
 
 def load_page(url: str):
-    #todo: add code to read url
+    # todo: add code to read url
     pass
 
+
 def scrape_page(page_contents: str):
-    #todo: analyze text
+    # todo: analyze text
     pass
