27dd52c0579879b460798597b854730aaf5dc1b9
jcastro61 <jcastro61@hotmail.com>
2019-01-21 17:47:14 -0500

    fix main

--- page_spider.py
+++ page_spider.py
@@ -15,7 +15,10 @@ def main(database: str, url_list_file: str):
         page_content = url_utilities.load_page(url=url)
         words = url_utilities.scrape_page(page_contents=page_content)
         big_word_list.extend(words)
-    database_utilities.save_words_to_database(database_path=database, word_list=big_word_list)
+
+    os.chdir(os.path.dirname(__file__))
+    path = os.path.join(os.getcwd(), database)
+    database_utilities.save_words_to_database(database_path=path, word_list=big_word_list)
 
 
 if __name__ == "__main__":
